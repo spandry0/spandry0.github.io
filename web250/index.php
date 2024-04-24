a36bcae0513ff30ac38cce9687c0ff2183607905
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-04-24 09:44:26 -0400

    index.php

--- web250/index.php
+++ web250/index.php
@@ -1,53 +1,44 @@
 <!DOCTYPE html>
-<html>
-<head>
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link href="./css/default.css" rel="stylesheet" type="text/css">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-             <title>Stephane Pandry || WEB250 ||Login</title>
+<html lang = "en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles/default.css">
+               <title>Stephane Pandry | WEB250 | Contract</title>
+  	
 </head>
 <body>
-  
-	   <header>
-<h1>Stephane Pandry |Antelpe| WEB250 | Home</h1>
-    </header>
-  <nav>
-<ul class="nav-links">
+	<?php include 'components/header.html'; ?>
+         <header>
+               <h1>Stephane Pandry | Antelope | WEB250 </h1>
+         </header>
+    <nav>
+           <ul class="nav-links">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="introduction.html">Introduction</a></li>
+                <li><a href="contract.html">Contract</a></li>
+                <li><a href="brand.html">Brand</a></li>
+            </ul>
+    </nav>
+    <main>
+	    <h2>Home</h2>
+        <p>
+            Welcome to my WEB250 website page! <br>You can find all the necessary information about me in the <a href=introduction.html>Introduction</a>. </P>
 
-	 <li><a href = "?p=../contents/home.php">Home</a><li> 
-         <li><a href = "?p=../contents/introduction.php">Introduction</a><li>
-         <li><a href = "?p=../contents/contract.php">Contract</a><li>
-         <li><a href = "?p=../contents/brand.php">brand</a><li>
-	 <li><a href = "?p=../contents/login.php">Login</a><li>
-      <ul>
-	</nav>
-<main>
-</header>
-<div class="login-container">
-        <h2>Login</h2>
-        <form action="login.php" method="POST">
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <input type="submit" value="Login">
-        </form>
-		</main>
+    </main>
     <footer> 
         <div class="nav-links">
             <nav>
-            <a href="https://github.com/spandry0">GitHub</a>
-            <a href="https://github.com/spandry0/spandry0.github.io">GitHub.io</a>
-            <a href="https://www.freecodecamp.org/learn/">freeCodeCamp</a>
-            <a href="https://www.codecademy.com/profiles/Spandry0">Codecademy</a>
-            <a href="https://jsfiddle.net/">JSFiddle</a>
-            <a href="https://www.linkedin.com/in/stephane-pandry-3b811a205/">LinkedIn</a>
+            <a href="https://github.com/spandry0">GitHub </a>
+            <a href="https://github.com/spandry0/spandry0.github.io">GitHub.io </a>
+            <a href="https://www.freecodecamp.org/learn">freeCodeCamp </a>
+            <a href="https://www.codecademy.com/profiles/Spandry0">Codecademy </a>
+            <a href="https://jsfiddle.net/">JSFiddle </a>
+            <a href="https://www.linkedin.com/in/stephane-pandry-3b811a205/">LinkedIn </a>
+	    <a href="https://github.com/spandry0/web250.io">Web250.oi </a>
             </nav>
             <br>
+         <div class="designedBy">
          
          </div>
-</footer>
-<p>&copy; 2024 Home Pyramid. All rights reserved.</p>  
-
-           </div>
          <div class="validationButtons">
             <a href="http://validator.w3.org/check?uri=referer" style = "text-decoration: none">
 		    <img src="images/html_validation_button.gif" alt="Validate HTML" /> 
@@ -57,5 +48,7 @@
 		</a> 
         </div>
     </footer>
+<?php include 'components/footer.html'; ?>
+    <script src="scripts/HTMLInclude.js"></script>
 </body>
 </html>
