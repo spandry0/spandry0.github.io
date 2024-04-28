ff1cf5315c4e23aae77192e2dee822e28f678902
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-04-28 18:31:53 -0400

    home.php

---
+++ web250/contents/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles/default.css">
+               <title>Stephane Pandry | WEB250 | Home</title>
+  	
+</head>
+<body>
+      
+    <main>
+      <main class="introMain">
+              <h2>Introduction</h2>
+        <div class="introList">
+	    <h2>Home</h2>
+        <p>
+            Welcome to my WEB250 website page! <br>You can find all the necessary information about me in the <a href="index.php?p=introduction">Introduction</a>. </P>
+
+    </main>
+         </div>
+</body>
+</html>
