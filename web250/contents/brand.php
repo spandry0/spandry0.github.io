0585d5723151005891410963073a4dec4d4481e3
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-04-29 15:20:53 -0400

    brand.php

---
+++ web250/contents/brand.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head><meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Stephane Pandry | WEB250 | Brand</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="styles/default.css">
+	<link rel="stylesheet" href="styles/brand.css">
+	
+               
+</head>
+<body>
+  <main>
+	      <h2>Brand</h2>
+	<h2>Concept</h2>
+           <p><i>" Talent. Genius. Creativity. "</i></p>
+            <h2>Pyramid</h2>
+	      
+      <div class="mainPyramid">
+	      
+        <p> Pyramid idea is to help children develop their talents: Expose them to new activities; Involve them in daily Activities; and Provide opportunities for deliberate practice.</p>
+         <br>
+
+        <div class="color">
+          <p class="brandP">Colors</p>
+          <div class="colorsBox">
+            <div class="color1">
+              <p>#0088dd</p>
+            </div>
+            <div class="color2">
+              <p>#413f3b</p>
+            </div>
+            <div class="color3">
+              <p>#66cccc</p>
+            </div>
+            <div class="color4">
+              <p>#efefef</p>
+             </div>
+    </main>
+  
+       </div>
+</body>
+</html>
