d0538ff351778cec2fe014d0393226984599231e
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-05-04 15:50:44 -0400

    introduction.php

--- web250/contents/introduction.php
+++ web250/contents/introduction.php
@@ -3,6 +3,7 @@
 <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" type="text/css" href="styles/default.css">
+      <link rel="stylesheet" href="styles/introduction.css">
             <title>Stephane Pandry's Small penguin | WEB250</title>
 </head>
             <body>
