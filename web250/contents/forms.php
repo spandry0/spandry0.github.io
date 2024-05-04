dc8a2eec83cbe27b3bd1add5e3cfa01588852ffc
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-05-04 14:57:52 -0400

    forms.php

--- web250/contents/forms.php
+++ web250/contents/forms.php
@@ -5,7 +5,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="styles/default.css">
-        <title>Stephane Pandry | WEB250 | Forms</title>
+        <title>Stephane Pandry's Small penguin | WEB250 Forms</title>
 </head>
 <body>
         <main>
