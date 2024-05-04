915ea535118e692fb1245e053558670a19d5136e
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-05-04 15:59:26 -0400

    forms.php

--- web250/contents/forms.php
+++ web250/contents/forms.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="styles/default.css">
         <title>Stephane Pandry's Small penguin | WEB250 Forms</title>
+  <link rel="stylesheet" href="styles/forms.css">
 </head>
 <body>
         <main>
