efc2c187afdcf009c0d0fbc9b5426386c1207d54
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-05-05 22:49:45 -0400

    demo.php

---
+++ web250/contents/demo.php
@@ -0,0 +1,11 @@
+<?php
+       $sOutputString = "<hr/>";
+
+
+for($iCounter = 1; $iCounter <= 1000; $iCounter++)
+{
+     $sOutputString .= $iCounter . ") Logovi got PHP running on my computer!! ";
+}
+
+
+echo ("<h5>" . $sOutputString . "</h5><hr/>");
