421fe1eca6eff0bed4085589b61bf047e864ca8a
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-05-05 23:37:16 -0400

    brand.php

--- web250/contents/brand.php
+++ web250/contents/brand.php
@@ -1,26 +1,14 @@
-<!DOCTYPE html>
-<html lang = "en">
-<head><meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Stephane Pandry's Small penguin | WEB250| Brand</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" type="text/css" href="styles/default.css">
-	<link rel="stylesheet" href="styles/brand.css">
-	
-               
-</head>
-<body>
-  <main>
+
 	      <h2>Brand</h2>
 	<h2>Concept</h2>
-           <p><i>" Talent. Genius. Creativity. "</i></p>
+           <p><i><em>" Talent. Genius. Creativity. "</em></i></p>
             <h2>Pyramid</h2>
 	      
       <div class="mainPyramid">
 	      
         <p> Pyramid idea is to help children develop their talents: Expose them to new activities; Involve them in daily Activities; and Provide opportunities for deliberate practice.</p>
          <br>
-
+<p id="center">
         <div class="color">
           <p class="brandP">Colors</p>
           <div class="colorsBox">
@@ -36,8 +24,7 @@
             <div class="color4">
               <p>#efefef</p>
              </div>
+<p id="center">
     </main>
   
        </div>
-</body>
-</html>
