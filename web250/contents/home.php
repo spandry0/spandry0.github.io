2704893694ad7d5b45e8d30ea4fe0165cfe3e3ef
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-05-05 23:14:22 -0400

    home.php

--- web250/contents/home.php
+++ web250/contents/home.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang = "en">
-<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" type="text/css" href="styles/default.css">
-               <title>Stephane Pandry's Small penguin | WEB250 | Home</title>
-  	
-</head>
-<body>
-      
-    <main>
-      <main class="introMain">
-              <h2>Introduction</h2>
-        <div class="introList">
-	    <h2>Home</h2>
-        <p>
-            Welcome to my WEB250 website page! You can find all the necessary information about me in the <a href="index.php?p=introduction">Introduction</a>. </P>
+<h2>Welcome to my Webpage.</h2>
 
-    </main>
-         </div>
-</body>
-</html>
+        <p>Welcome to my WEB250 website page! You can find all the necessary information about me in the <a href="index.php?p=introduction">Introduction</a>.</P>
+
