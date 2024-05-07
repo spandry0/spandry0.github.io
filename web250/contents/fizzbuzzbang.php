83ec2dc96f2f96bc0c8a84f8902acb71b5405bd2
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-05-07 01:59:29 -0400

    fizzbuzzbang.php

--- web250/contents/fizzbuzzbang.php
+++ web250/contents/fizzbuzzbang.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles/default.css">
+    <title>Stephane Pandry's Small Penguin | WEB250 | Course Contract</title>
+</head>
+<body>
+<header>
+    <h1>Stephane Pandry's Small Penguin | WEB250 </h1>
+</header>
+<nav>
+    <ul class="nav-links">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="introduction.php">Introduction</a></li>
+        <li><a href="contract.php">Contract</a></li><li><a href="brand.html">Brand</a></li>
+	<li><a href="forms.php">Forms</a></li>
+        <li><a href="FizzBuzzBang.php">FizzBuzz</a></li>
+       
+    </ul>
+</nav>
+
 <h2>FizzBuzzBang</h2>
 
   <?php
@@ -65,5 +87,31 @@
         Number for Bang: <input type="number" name="bang_num" value="<?php echo $bang_num; ?>"><br><br>
         <input type="submit" value="Submit">
     </form>
+<footer> 
+    <div class="nav-links">
+        <nav>
+            <a href="https://github.com/spandry0">GitHub </a>
+            <a href="https://github.com/spandry0/spandry0.github.io">GitHub.io </a>
+            <a href="https://github.com/spandry0/web250.io">WEB250.oi </a>
+            <a href="https://www.freecodecamp.org/learn">freeCodeCamp </a>
+            <a href="https://www.codecademy.com/profiles/Spandry0">Codecademy </a>
+            <a href="https://jsfiddle.net/">JSFiddle </a>
+            <a href="https://www.linkedin.com/in/stephane-pandry-3b811a205/">LinkedIn </a>
+        </nav>
+        <br>
+        <div class="designedBy">
+         
+        </div>
+        <div class="validationButtons">
+            <a href="http://validator.w3.org/check?uri=referer" style="text-decoration: none">
+                <img src="images/html_validation_button.gif" alt="Validate HTML" />
+            </a>
+            <a href="http://jigsaw.w3.org/css-validator/check/referer" style="text-decoration: none">
+                <img src="images/validation_button_css_rectangle.gif" alt="Validate CSS" />
+            </a> 
+        </div>
+    </div>
+</footer>
+
 </body>
 </html>
