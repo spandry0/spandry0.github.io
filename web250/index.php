af368e2c50e82874ffa967d20f069872a90ede0d
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-05-07 00:17:42 -0400

    index.php

--- web250/index.php
+++ web250/index.php
@@ -2,22 +2,50 @@
 <html lang = "en">
 <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" type="text/css" href="styles/default.css">
-               <title>Stephane Pandry's Small penguin | WEB250| Contract</title>
+               <title>Stephane Pandry's Small Penguin | WEB250| Contract</title>
   	
 </head>
 <body>
 	<?php include 'components/header.html'; ?>
          <header>
-               <h1>Stephane Pandry's Small penguin | WEB250 |</h1>
+               <h1>Stephane Pandry's Small Penguin | WEB250 |</h1>
          </header>
     <nav>
            <ul class="nav-links">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="introduction.html">Introduction</a></li>
-                <li><a href="contract.html">Contract</a></li>
-                <li><a href="brand.html">Brand</a></li>
+		<li><a href = "index.html">Home</a></li> 
+		<li><a href = "?p=home">PHP Home</a></li>
+       		<li><a href = "?p=introduction">Introduction</a></li>
+        	<li><a href = "?p=contract">Contract</a></li>
+        	<li><a href = "?p=brand">Brand</a></li>
+		<li><a href = "?p=fizzbuzzbang">FizzBuzzBang</a></li> 
+		<li><a href = "?p=login">Login</a></li>
+		<li><a href = "?p=forms">Forms</a></li>
             </ul>
     </nav>
+	<p id="center">
+
+	<!-- dynamic content goes here -->
+
+<?php
+if($_GET) 
+{
+	//echo("<h1>it found it<h1>");
+	$sPage = $_GET["p"];
+}
+else  
+{
+	//echo("<h1>it failed it<h1>");
+    $sPage = "home";
+}
+
+
+	//echo ("<h1>You picked the page: " . $sPage . "<h1>"); 
+	
+	$sPage = "contents/" . $sPage . ".php";
+	include($sPage);
+?>
+
+</main></p></div>
     <main>
 	    <h2>Home</h2>
         <p>
@@ -29,11 +57,12 @@
             <nav>
             <a href="https://github.com/spandry0">GitHub </a>
             <a href="https://github.com/spandry0/spandry0.github.io">GitHub.io </a>
+	    <a href="https://github.com/spandry0/web250.io">WEB250.oi </a>
             <a href="https://www.freecodecamp.org/learn">freeCodeCamp </a>
             <a href="https://www.codecademy.com/profiles/Spandry0">Codecademy </a>
             <a href="https://jsfiddle.net/">JSFiddle </a>
             <a href="https://www.linkedin.com/in/stephane-pandry-3b811a205/">LinkedIn </a>
-	    <a href="https://github.com/spandry0/web250.io">Web250.oi </a>
+	  
             </nav>
             <br>
          <div class="designedBy">
