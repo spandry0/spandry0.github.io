a3d40c3e2dcffa71d972396e707c563df35372c3
STEPHANE PANDRY <122483914+spandry0@users.noreply.github.com>
2024-05-07 18:09:16 -0400

    home.php

--- web250/contents/home.php
+++ web250/contents/home.php
@@ -1,4 +1,3 @@
-<h2>Welcome to my Webpage.</h2>
-
-        <p>Welcome to my WEB250 website page! You can find all the necessary information about me in the <a href="index.php?p=introduction">Introduction</a>.</P>
+   <h2>Home</h2>
+        <p>Welcome to my WEB250 website page! You can find all the necessary information about me in the <a href="index.php?page=introduction"></P>
 
